47943d532dc3b77a6a07b4e0df7e6ba04f08068b
qdwang <wangqidong@gmail.com>
2022-09-23 11:30:02 +0800

    add doc; raname files and variables

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,9 @@
+//! Parses the data bytes by the rule and collects the values.
+//! 
+//! 
 use super::*;
-use exif::*;
 use parsed_info::*;
+use rule::*;
 use std::collections::HashMap;
 use utility::GetNumFromBytes;
 use value::*;
@@ -18,8 +21,8 @@ pub enum Error {
     InvalidTiffHeaderByteOrder(u16),
     #[error("The tag {0:#02x?} was not found.")]
     TagNotFound(u16),
-    #[error("The start task should be Tiff or JPEG.")]
-    InvalidStartTask,
+    #[error("The start rule should be Tiff or JPEG.")]
+    InvalidStartRule,
     #[error("Scan failed to find '{0:#02x?}'.")]
     ScanFailed(&'static [u8]),
 }
@@ -42,14 +45,14 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse(buffer: &[u8], task: &exif::ExifTask) -> Result<ParsedInfo, Error> {
+    pub fn parse(buffer: &[u8], rule: &rule::ParsingRule) -> Result<ParsedInfo, Error> {
         let content = HashMap::new();
-        Self::get_info_with_content(buffer, task, content)
+        Self::get_info_with_content(buffer, rule, content)
     }
 
     fn get_info_with_content(
         buffer: &[u8],
-        task: &exif::ExifTask,
+        rule: &rule::ParsingRule,
         mut content: HashMap<String, Value>,
     ) -> Result<ParsedInfo, Error> {
         let buffer = if buffer[..2] == [0xff, 0xd8] {
@@ -66,32 +69,32 @@ impl<'a> Parser<'a> {
             entries: HashMap::new(),
             next_offset: 0,
         };
-        decoder.run_task_body(task, &mut content)?;
+        decoder.run_rule_body(rule, &mut content)?;
         Ok(ParsedInfo {
             is_le: decoder.is_le,
             content,
         })
     }
 
-    fn is_tasks_in_ifd(tasks: &[exif::ExifTask]) -> bool {
-        tasks.iter().any(|x| {
+    fn is_rules_in_ifd(rules: &[rule::ParsingRule]) -> bool {
+        rules.iter().any(|x| {
             matches!(
                 x,
-                exif::ExifTask::Jump {
+                rule::ParsingRule::Jump {
                     tag: _,
                     is_optional: _,
-                    tasks: _
-                } | exif::ExifTask::TagItem {
+                    rules: _
+                } | rule::ParsingRule::TagItem {
                     tag: _,
                     name: _,
                     len: _,
                     is_optional: _,
                     is_value_u16: _,
-                } | exif::ExifTask::SonyDecrypt {
+                } | rule::ParsingRule::SonyDecrypt {
                     offset_tag: _,
                     len_tag: _,
                     key_tag: _,
-                    tasks: _
+                    rules: _
                 }
             )
         })
@@ -128,13 +131,13 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn run_remain_tasks(
+    fn run_remain_rules(
         &mut self,
-        tasks: &[exif::ExifTask],
+        rules: &[rule::ParsingRule],
         content: &mut HashMap<String, Value>,
     ) -> Result<(), Error> {
         // IFD entry check
-        if Parser::is_tasks_in_ifd(tasks) {
+        if Parser::is_rules_in_ifd(rules) {
             let entry_count = self.buffer.u16(self.is_le, self.offset) as usize;
             self.offset += 2;
 
@@ -149,19 +152,19 @@ impl<'a> Parser<'a> {
             self.next_offset = self.buffer.u32(self.is_le, self.offset + entry_count * 12) as usize;
         }
 
-        for task in tasks.iter() {
-            self.run_task_body(task, content)?;
+        for rule in rules.iter() {
+            self.run_rule_body(rule, content)?;
         }
         Ok(())
     }
-    fn run_task_body(
+    fn run_rule_body(
         &self,
-        task: &exif::ExifTask,
+        rule: &rule::ParsingRule,
         content: &mut HashMap<String, Value>,
     ) -> Result<(), Error> {
-        match task {
+        match rule {
             // blocks
-            exif::ExifTask::Tiff(tasks) => {
+            rule::ParsingRule::Tiff(rules) => {
                 let is_le = if self.offset == 0 {
                     self.is_le
                 } else {
@@ -176,9 +179,9 @@ impl<'a> Parser<'a> {
                     entries: HashMap::new(),
                     next_offset: 0,
                 };
-                new_parser.run_remain_tasks(tasks, content)?;
+                new_parser.run_remain_rules(rules, content)?;
             }
-            &exif::ExifTask::Condition {
+            &rule::ParsingRule::Condition {
                 cond,
                 ref left,
                 ref right,
@@ -202,11 +205,11 @@ impl<'a> Parser<'a> {
                     CondType::EXIST => content.get(field).is_some(),
                 };
 
-                for task in if result { left } else { right }.iter() {
-                    self.run_task_body(task, content)?;
+                for rule in if result { left } else { right }.iter() {
+                    self.run_rule_body(rule, content)?;
                 }
             }
-            &exif::ExifTask::JumpNext(ref tasks) => {
+            &rule::ParsingRule::JumpNext(ref rules) => {
                 let mut new_parser = Parser {
                     is_le: self.is_le,
                     buffer: self.buffer,
@@ -214,12 +217,12 @@ impl<'a> Parser<'a> {
                     entries: HashMap::new(),
                     next_offset: 0,
                 };
-                new_parser.run_remain_tasks(tasks, content)?;
+                new_parser.run_remain_rules(rules, content)?;
             }
-            &exif::ExifTask::Jump {
+            &rule::ParsingRule::Jump {
                 tag,
                 is_optional,
-                ref tasks,
+                ref rules,
             } => {
                 let offset = self.read_u32value_from_entries(tag, None);
                 match (offset, is_optional) {
@@ -231,16 +234,16 @@ impl<'a> Parser<'a> {
                             entries: HashMap::new(),
                             next_offset: 0,
                         };
-                        new_parser.run_remain_tasks(tasks, content)?;
+                        new_parser.run_remain_rules(rules, content)?;
                     }
                     (Err(e), false) => Err(e)?,
                     _ => {}
                 }
             }
-            &exif::ExifTask::Scan {
+            &rule::ParsingRule::Scan {
                 marker,
                 name,
-                ref tasks,
+                ref rules,
             } => {
                 let &(offset, _) = &self.buffer[self.offset..]
                     .windows(marker.len())
@@ -260,13 +263,13 @@ impl<'a> Parser<'a> {
                     entries: HashMap::new(),
                     next_offset: 0,
                 };
-                new_parser.run_remain_tasks(tasks, content)?;
+                new_parser.run_remain_rules(rules, content)?;
             }
-            &exif::ExifTask::Offset(ref offset, ref tasks) => {
+            &rule::ParsingRule::Offset(ref offset, ref rules) => {
                 let new_offset = match offset {
                     OffsetType::Bytes(0) => {
-                        for task in tasks.iter() {
-                            self.run_task_body(task, content)?;
+                        for rules in rules.iter() {
+                            self.run_rule_body(rules, content)?;
                         }
                         return Ok(());
                     }
@@ -290,13 +293,13 @@ impl<'a> Parser<'a> {
                     entries: HashMap::new(),
                     next_offset: 0,
                 };
-                new_parser.run_remain_tasks(tasks, content)?;
+                new_parser.run_remain_rules(rules, content)?;
             }
-            &exif::ExifTask::SonyDecrypt {
+            &rule::ParsingRule::SonyDecrypt {
                 offset_tag,
                 len_tag,
                 key_tag,
-                ref tasks,
+                ref rules,
             } => {
                 let offset = self.read_u32value_from_entries(offset_tag, None)? as usize;
                 let len = self.read_u32value_from_entries(len_tag, None)? as usize;
@@ -315,10 +318,10 @@ impl<'a> Parser<'a> {
                     entries: HashMap::new(),
                     next_offset: 0,
                 };
-                new_parser.run_remain_tasks(tasks, content)?;
+                new_parser.run_remain_rules(rules, content)?;
             }
             // items
-            &exif::ExifTask::TagItem {
+            &rule::ParsingRule::TagItem {
                 tag,
                 name,
                 len,
@@ -342,7 +345,7 @@ impl<'a> Parser<'a> {
                     _ => {}
                 };
             }
-            &exif::ExifTask::OffsetItem {
+            &rule::ParsingRule::OffsetItem {
                 offset,
                 name,
                 ref t,
