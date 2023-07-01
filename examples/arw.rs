3bd698b8ce5bec62e7bf4f3ee8f147911560ad6c
qdwang <wangqidong@gmail.com>
2023-07-01 11:07:24 +0800

    move tests to examples

---
+++ examples/arw.rs
@@ -0,0 +1,75 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::{collections::HashMap, fs::File};
+use quickexif::log_helper::*;
+
+mod sony_tags {
+    #![allow(non_upper_case_globals)]
+    use quickexif::gen_tags_info;
+
+    gen_tags_info!(
+        0 -> 0xc634 -> 0 {}
+        0 -> 0xc634 -> 0 -> 0x7200 -> 0xffff {
+            0x7310 black_level
+            0x7312 white_balance
+            0x787f white_level
+        }
+        0 {
+            0x010f make
+            0x0110 model
+            0x0112 orientation
+            0x0201 preview_offset
+            0x0202 preview_len
+        }
+        0 -> 0x8769 -> 0 {
+            0x9102 compressed_bps
+        }
+        0 -> 0x014a -> 0 {
+            0x0103 compression
+            0x0100 width
+            0x0101 height
+            0x0102 bps
+            0x828e cfa_pattern
+            0x0111 strip
+            0x0117 strip_len
+            0x7010 tone_curve_addr
+            0xc61f crop_xy
+            0xc620 crop_wh
+        }
+    );
+}
+
+fn main() -> LogResult<()> {
+    let sample = "examples/samples/sample1.ARW";
+    let f = q!(File::open(sample));
+
+    let result = q!(quickexif::parse_exif(f, sony_tags::PATH_LST, Some((0, 1))));
+
+    println!("{:?}", result.get(sony_tags::make).and_then(|x| x.str()));
+    println!("{:?}", result.get(sony_tags::model).and_then(|x| x.str()));
+
+    println!("{:?}", result.get(sony_tags::orientation).map(|x| x.u16()));
+    println!("{:x?}", result.get(sony_tags::preview_offset).map(|x| x.u32()));
+    println!("{:?}", result.get(sony_tags::preview_len).map(|x| x.u32()));
+
+    println!("{:?}", result.get(sony_tags::compressed_bps).and_then(|x| x.r64s()));
+    println!("{:?}", result.get(sony_tags::compression).map(|x| x.u16()));
+    println!("{:?}", result.get(sony_tags::width).map(|x| x.u16()));
+    println!("{:?}", result.get(sony_tags::height).map(|x| x.u16()));
+    println!("{:?}", result.get(sony_tags::bps).map(|x| x.u16()));
+    println!("{:x?}", result.get(sony_tags::cfa_pattern).map(|x| x.raw()));
+    println!("{:x?}", result.get(sony_tags::strip).map(|x| x.u32()));
+    println!("{:x?}", result.get(sony_tags::strip_len).map(|x| x.u32()));
+    println!("{:?}", result.get(sony_tags::tone_curve_addr).and_then(|x| x.u16s()));
+
+    println!("{:?}", result.get(sony_tags::crop_xy).and_then(|x| x.u32s()));
+    println!("{:?}", result.get(sony_tags::crop_wh).and_then(|x| x.u32s()));
+
+    println!("{:?}", result.get(sony_tags::black_level).and_then(|x| x.u16s()));
+
+    println!("{:?}", result.get(sony_tags::white_balance).and_then(|x| x.u16s()));
+    println!("{:?}", result.get(sony_tags::white_level).and_then(|x| x.u16s()));
+
+    Ok(())
+}
---
+++ examples/orf.rs
@@ -0,0 +1,56 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::{collections::HashMap, fs::File};
+use quickexif::log_helper::*;
+
+mod olympus_tags {
+    #![allow(non_upper_case_globals)]
+    use quickexif::gen_tags_info;
+
+    gen_tags_info!(
+        0 {
+            0x0112 orientation
+            0x0100 width
+            0x0101 height
+            0x0111 strip
+            0x0117 strip_len
+        }
+        0 -> 0x8769 -> 0 {
+            0xa302 cfa_pattern
+        }
+        0 -> 0x8769 -> 0 -> 0x927c -> 0 {}
+        0 -> 0x8769 -> 0 -> 0x927c -> 0 -> 0x2040 -> 0 {
+            0x0611 bps
+            0x0612 crop_left
+            0x0613 crop_top
+            0x0614 crop_width
+            0x0615 crop_height
+            0x0100 white_balance
+            0x0600 black_level
+        }
+    );
+}
+
+fn main() -> LogResult<()> {
+    let sample = "examples/samples/sample0.ORF";
+    let f = q!(File::open(sample));
+
+    let result = q!(quickexif::parse_exif(f, olympus_tags::PATH_LST, None));
+
+    println!("{:?}", result.get(olympus_tags::orientation).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::width).map(|x| x.u32()));
+    println!("{:?}", result.get(olympus_tags::height).map(|x| x.u32()));
+    println!("{:?}", result.get(olympus_tags::strip).map(|x| x.u32()));
+    println!("{:?}", result.get(olympus_tags::strip_len).map(|x| x.u32()));
+    println!("{:?}", result.get(olympus_tags::cfa_pattern).map(|x| x.raw()));
+    println!("{:?}", result.get(olympus_tags::bps).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::crop_left).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::crop_top).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::crop_width).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::crop_height).map(|x| x.u16()));
+    println!("{:?}", result.get(olympus_tags::white_balance).and_then(|x| x.u16s()));
+    println!("{:?}", result.get(olympus_tags::black_level).and_then(|x| x.u16s()));
+
+    Ok(())
+}
