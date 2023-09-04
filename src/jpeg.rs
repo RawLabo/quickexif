a247f5508da69c9d7140b31f594440ecb4ca69f7
qdwang <wangqidong@gmail.com>
2023-09-04 16:24:36 +0800

    add sos parsing in jpeg

--- src/jpeg.rs
+++ src/jpeg.rs
@@ -49,10 +49,82 @@ impl<'a> DHT<'a> {
 }
 
 #[derive(Debug)]
+pub struct SOS<'a> {
+    /// [2bytes for 1 component: Scan component selector + DC entropy coding table destination selector + AC entropy coding table destination selector]
+    scan_header: Vec<(u8, u8, u8)>,
+    /// Start of spectral or predictor selection
+    ss: u8,
+    /// End of spectral selection
+    se: u8,
+    /// Successive approximation bit position high
+    ah: u8,
+    /// Successive approximation bit position low or point transform
+    al: u8,
+
+    body: &'a [u8],
+}
+impl<'a> Default for SOS<'a> {
+    fn default() -> Self {
+        SOS {
+            scan_header: vec![],
+            ss: 0,
+            se: 0,
+            ah: 0,
+            al: 0,
+            body: &[],
+        }
+    }
+}
+impl<'a> SOS<'a> {
+    fn parse_from_bytes(bytes: &'a [u8]) -> Result<Self, Report> {
+        let cursor = &mut 0;
+        bytes.u16(cursor).to_report()?; // get scan header length
+
+        let components = bytes.u8(cursor).to_report()?;
+        let mut scan_header = Vec::with_capacity(components as usize);
+        for _ in 0..components {
+            // Scan component selector
+            let csj = bytes.u8(cursor).to_report()?;
+            let entropy_selector = bytes.u8(cursor).to_report()?;
+            // DC entropy coding table destination selector
+            let tdj = entropy_selector >> 4;
+            // AC entropy coding table destination selector
+            let taj = entropy_selector & 0x0f;
+            scan_header.push((csj, tdj, taj));
+        }
+
+        let ss = bytes.u8(cursor).to_report()?;
+        let se = bytes.u8(cursor).to_report()?;
+
+        let successive_approximation = bytes.u8(cursor).to_report()?;
+        let ah = successive_approximation >> 4;
+        let al = successive_approximation & 0x0f;
+
+        let body = &bytes.slice(cursor, bytes.len() - *cursor - 2).to_report()?;
+        let tail = bytes.u16(cursor).to_report()?;
+
+        if tail != 0xffd9 {
+            Err(Error::InvalidTail(tail)).to_report()
+        } else {
+            Ok(SOS {
+                scan_header,
+                ss,
+                se,
+                ah,
+                al,
+                body,
+            })
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct JPEG<'a> {
     dqt: &'a [u8],
     sof: (u8, &'a [u8]),
     dht: Vec<DHT<'a>>,
+    data: &'a [u8],
+    sos: SOS<'a>,
 }
 impl<'a> Default for JPEG<'a> {
     fn default() -> Self {
@@ -60,6 +132,8 @@ impl<'a> Default for JPEG<'a> {
             dqt: &[],
             sof: (0, &[]),
             dht: vec![],
+            data: &[],
+            sos: SOS::default(),
         }
     }
 }
@@ -70,6 +144,8 @@ pub enum Error {
     IndexError(usize),
     #[error("Invalid header for JPEG: {0}")]
     InvalidHeader(u16),
+    #[error("JPEG tail error: {0:x}")]
+    InvalidTail(u16),
 }
 
 impl<'a> JPEG<'a> {
@@ -99,6 +175,10 @@ impl<'a> JPEG<'a> {
                     let dhts = DHT::parse_from_bytes(&bytes.slice(cursor, size - 2).to_report()?)
                         .to_report()?;
                     jpeg.dht.extend(dhts);
+                }
+                0xffda => {
+                    let sos = SOS::parse_from_bytes(&bytes[*cursor..])?;
+                    jpeg.sos = sos;
                 }
                 _ => {
                     break;
@@ -136,6 +216,7 @@ impl Read4JPEG for [u8] {
     fn slice<'a>(&'a self, cursor: &mut usize, size: usize) -> Result<&'a [u8], Error> {
         let range = *cursor..*cursor + size;
         *cursor += size;
-        self.get(range).ok_or_else(|| Error::IndexError(*cursor - size))
+        self.get(range)
+            .ok_or_else(|| Error::IndexError(*cursor - size))
     }
 }
